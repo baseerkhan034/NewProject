4c8c2710c687aa4781b69d8565e885df923d006a
baseerkhan034 <126024886+baseerkhan034@users.noreply.github.com>
2023-06-04 13:36:34 -0400

    Update MainProcess.java

--- MainProcess.java
+++ MainProcess.java
@@ -69,6 +69,8 @@ public class MainProcess extends Thread implements Runnable{
 		executor.execute(task2);		
 		executor.execute(task3);
 		
+		System.out.println("this is the thread example . . . ");
+		
 		
 		
 	}
